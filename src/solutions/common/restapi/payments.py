e43c5767db6acde5fab70b38270b8ba67a36c2e4
Ruben Mattan <rubenmattan@gmail.com>
2017-10-13 14:51:23 +0200

    Add api calls for payconiq payments

---
+++ src/solutions/common/restapi/payments.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Mobicage NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @@license_version:1.2@@
+
+
+import json
+
+from mcfw.restapi import rest
+from mcfw.rpc import returns, arguments
+from rogerthat.rpc import users
+from rogerthat.service.api.payments import get_provider, put_provider
+
+
+@rest('/common/payments/payconiq', 'get', read_only_access=True, silent_result=True)
+@returns()
+@arguments()
+def rest_get_payconiq_settings():
+    service_identity = users.get_current_session().service_identity
+    return get_provider(u'payconiq', service_identity)
+
+
+@rest('/common/payments/payconiq', 'post')
+@returns()
+@arguments(merchant_id=unicode, jwt=unicode, online_key=unicode)
+def rest_put_news_item(merchant_id, jwt, online_key):
+    service_identity = users.get_current_session().service_identity
+
+    settings = {
+        u'merchant_id': merchant_id,
+        u'jwt': jwt,
+        u'online_key': online_key
+    }
+    return put_provider(u'payconiq', json.dumps(settings), service_identity)
